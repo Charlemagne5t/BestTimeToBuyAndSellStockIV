59000498233ab58b28debec335a655cb8da1c93e
alek <wait82985@gmail.com>
2023-06-20 16:39:56 +0600

    problem solved

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,5 +1,31 @@
 public class Solution {
     public int maxProfit(int k, int[] prices) {
-        return 0;
+        Integer[][][] memo = new Integer[k + 1][prices.length + 1][2];
+        return dfs(k, prices, 0, 1, memo);
+    }
+
+    private int dfs(int k, int[] prices, int i, int ableToBuy, Integer[][][] memo) {
+        if (i == prices.length) {
+            return 0;
+        }
+        if (k == 0) return 0;
+
+        if (memo[k][i][ableToBuy] != null) {
+            return memo[k][i][ableToBuy];
+        }
+        int result = 0;
+
+        if (ableToBuy == 1) {
+            int doNothing = dfs(k, prices, i + 1, 1, memo);
+            int buy = dfs(k, prices, i + 1, 0, memo) - prices[i];
+            result = Math.max(doNothing, buy);
+        }
+        if (ableToBuy == 0) {
+            int doNothing = dfs(k, prices, i + 1, 0, memo);
+            int sell = dfs(k - 1, prices, i + 1, 1, memo) + prices[i];
+            result = Math.max(doNothing, sell);
+        }
+        memo[k][i][ableToBuy] = result;
+        return result;
     }
 }
